724c82114c0803c5a650e0517336ee4d702fdf40
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2026-07-17 15:25:52 -0300

    Termine algunas cosas pendientes de factibilidad

--- Clases/caja.py
+++ Clases/caja.py
@@ -1,8 +1,6 @@
-from Clases.producto import Producto
-
 class Caja:
     def __init__(self, caja_id, dim_interior_ancho, dim_interior_largo, dim_interior_alto, grosor_mm, 
-                 volumen_buenos_aires, volumen_curitiba, volumen_santiago, volumen_monterrey, volumen_bakersfield,
+                 compra_buenos_aires, compra_curitiba, compra_santiago, compra_monterrey, compra_bakersfield,
                  costo_unitario):
         
         self.caja_id = caja_id
@@ -15,11 +13,11 @@ class Caja:
         self.dim_exterior_largo = dim_interior_largo + 2 * grosor_mm
         self.dim_exterior_alto = dim_interior_alto + 2 * grosor_mm
         
-        self.volumen_buenos_aires = volumen_buenos_aires
-        self.volumen_curitiba = volumen_curitiba
-        self.volumen_santiago = volumen_santiago
-        self.volumen_monterrey = volumen_monterrey
-        self.volumen_bakersfield = volumen_bakersfield
+        self.unidades_buenos_aires = compra_buenos_aires
+        self.unidades_curitiba = compra_curitiba
+        self.unidades_santiago = compra_santiago
+        self.unidades_monterrey = compra_monterrey
+        self.unidades_bakersfield = compra_bakersfield
         
         self.costo_unitario = costo_unitario
         
@@ -31,10 +29,10 @@ class Caja:
         self.dim_exterior_largo = self.dim_interior_largo + 2 * grosor_mm
         self.dim_exterior_alto = self.dim_interior_alto + 2 * grosor_mm
     
-    def dimension_interna(self):
+    def volumen_interno(self):
         return self.dim_interior_alto * self.dim_interior_ancho * self.dim_interior_largo
     
-    def dimension_externa(self):
+    def volumen_externo(self):
         return self.dim_exterior_alto * self.dim_exterior_ancho * self.dim_exterior_largo   
     
     def perimetro(self):
@@ -46,9 +44,9 @@ class Caja:
         ect = ect_por_grosor[self.grosor_mm]
         return ect / self.perimetro() / 9.81
 
-    def volumen_total(self):
-        return (self.volumen_buenos_aires + self.volumen_curitiba + self.volumen_santiago +
-                self.volumen_monterrey + self.volumen_bakersfield)
+    def unidades_total(self):
+        return (self.unidades_buenos_aires + self.unidades_curitiba + self.unidades_santiago +
+                self.unidades_monterrey + self.unidades_bakersfield)
         
     def es_asignable_por_dimension(self, producto):
         entra = (producto.dim_producto_alto <= self.dim_interior_alto and
@@ -62,11 +60,11 @@ class Caja:
     def asignar_producto(self, producto):
         if self.es_asignable_por_dimension(producto):
             self.productos_asignados.append(producto)
-            self.volumen_buenos_aires -= producto.volumen_buenos_aires
-            self.volumen_curitiba -= producto.volumen_curitiba
-            self.volumen_santiago -= producto.volumen_santiago
-            self.volumen_monterrey -= producto.volumen_monterrey
-            self.volumen_bakersfield -= producto.volumen_bakersfield
+            self.unidades_buenos_aires -= producto.produccion_buenos_aires
+            self.unidades_curitiba -= producto.produccion_curitiba
+            self.unidades_santiago -= producto.produccion_santiago
+            self.unidades_monterrey -= producto.produccion_monterrey
+            self.unidades_bakersfield -= producto.produccion_bakersfield
         else:
             print("No es una caja asignable para el producto.")
     
@@ -86,7 +84,3 @@ class Caja:
                 f"Int: {self.dim_interior_ancho}x{self.dim_interior_largo}x{self.dim_interior_alto}mm | "
                 f"Ext: {self.dim_exterior_ancho}x{self.dim_exterior_largo}x{self.dim_exterior_alto}mm | "
                 f"Grosor: {self.grosor_mm}mm>")
-
-    @classmethod
-    def buscar_por_id(cls, caja_id):
-        return cls._todas_las_cajas.get(caja_id)
--- Clases/producto.py
+++ Clases/producto.py
@@ -1,6 +1,6 @@
 class Producto:
-    def __init__(self, codigo_producto, cantidad_paquetes, peso_paquete, volumen_buenos_aires, volumen_curitiba, 
-                 volumen_santiago, volumen_monterrey, volumen_bakersfield, dim_producto_ancho, dim_producto_largo,
+    def __init__(self, codigo_producto, cantidad_paquetes, peso_paquete, produccion_buenos_aires, produccion_curitiba, 
+                 produccion_santiago, produccion_monterrey, produccion_bakersfield, dim_producto_ancho, dim_producto_largo,
                  dim_producto_alto):
         
         self.codigo_producto = codigo_producto
@@ -8,21 +8,21 @@ class Producto:
         self.peso_paquete = peso_paquete
         self.peso_caja = cantidad_paquetes * peso_paquete
         
-        self.volumen_buenos_aires = volumen_buenos_aires
-        self.volumen_curitiba = volumen_curitiba
-        self.volumen_santiago = volumen_santiago
-        self.volumen_monterrey = volumen_monterrey
-        self.volumen_bakersfield = volumen_bakersfield
+        self.produccion_buenos_aires = produccion_buenos_aires
+        self.produccion_curitiba = produccion_curitiba
+        self.produccion_santiago = produccion_santiago
+        self.produccion_monterrey = produccion_monterrey
+        self.produccion_bakersfield = produccion_bakersfield
         
         self.dim_producto_ancho = dim_producto_ancho
         self.dim_producto_largo = dim_producto_largo
         self.dim_producto_alto = dim_producto_alto
 
-    def volumen_total(self):
-        return (self.volumen_buenos_aires + self.volumen_curitiba + self.volumen_santiago +
-                self.volumen_monterrey + self.volumen_bakersfield)
+    def produccion_total(self):
+        return (self.produccion_buenos_aires + self.produccion_curitiba + self.produccion_santiago +
+                self.produccion_monterrey + self.produccion_bakersfield)
 
     def __repr__(self):
         return (f"<Producto {self.codigo_producto} | "
                 f"Dim Prod: {self.dim_producto_ancho}x{self.dim_producto_largo}x{self.dim_producto_alto}mm | "
-                f"Volumen Total: {self.volumen_total()}>")
+                f"Volumen Total: {self.produccion_total()}>")
