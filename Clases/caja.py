a9d0daab74e534ba97afad7aedfb63f6a1ce6ed4
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2026-07-20 22:42:42 -0300

    Reorganice un poco la clase caja

--- Clases/caja.py
+++ Clases/caja.py
@@ -1,5 +1,17 @@
+def calcular_descuento_por_volumen(volumen):
+    if volumen < 20000:
+        return 0.10   # +10% markup
+    elif volumen < 50000:
+        return 0.00   # sin descuento
+    elif volumen < 100000:
+        return -0.10  # -10% descuento
+    elif volumen < 500000:
+        return -0.20  # -20% descuento
+    else:
+        return -0.30  # -30% descuento (máximo)
+
 class Caja:
-    def __init__(self, caja_id, dim_interior_ancho, dim_interior_largo, dim_interior_alto, grosor_mm, 
+    def __init__(self, caja_id, dim_interior_ancho, dim_interior_largo, dim_interior_alto, 
                  compra_buenos_aires, compra_curitiba, compra_santiago, compra_monterrey, compra_bakersfield,
                  costo_unitario):
         
@@ -8,10 +20,10 @@ class Caja:
         self.dim_interior_largo = dim_interior_largo
         self.dim_interior_alto = dim_interior_alto
         
-        self.grosor_mm = grosor_mm
-        self.dim_exterior_ancho = dim_interior_ancho + 2 * grosor_mm
-        self.dim_exterior_largo = dim_interior_largo + 2 * grosor_mm
-        self.dim_exterior_alto = dim_interior_alto + 2 * grosor_mm
+        self.grosor_mm = 0.0
+        self.dim_exterior_ancho = 0.0
+        self.dim_exterior_largo = 0.0
+        self.dim_exterior_alto = 0.0
         
         self.unidades_buenos_aires = compra_buenos_aires
         self.unidades_curitiba = compra_curitiba
@@ -19,7 +31,18 @@ class Caja:
         self.unidades_monterrey = compra_monterrey
         self.unidades_bakersfield = compra_bakersfield
         
+        self.unidades_buenos_aires_usadas = 0
+        self.unidades_curitiba_usadas = 0
+        self.unidades_santiago_usadas = 0
+        self.unidades_monterrey_usadas = 0
+        self.unidades_bakersfield_usadas = 0
+        
         self.costo_unitario = costo_unitario
+        self.descuento_buenos_aires = 0
+        self.descuento_curitiba = 0
+        self.descuento_santiago = 0
+        self.descuento_monterrey = 0
+        self.descuento_bakersfield = 0
         
         self.productos_asignados = []
             
@@ -60,27 +83,35 @@ class Caja:
     def asignar_producto(self, producto):
         if self.es_asignable_por_dimension(producto):
             self.productos_asignados.append(producto)
-            self.unidades_buenos_aires -= producto.produccion_buenos_aires
-            self.unidades_curitiba -= producto.produccion_curitiba
-            self.unidades_santiago -= producto.produccion_santiago
-            self.unidades_monterrey -= producto.produccion_monterrey
-            self.unidades_bakersfield -= producto.produccion_bakersfield
+            plantas = ['buenos_aires', 'curitiba', 'santiago', 'monterrey', 'bakersfield']
+            for planta in plantas:
+                self.__dict__[f'unidades_{planta}_usadas'] += producto.__dict__[f'produccion_{planta}']
+                self.__dict__[f'descuento_{planta}'] = calcular_descuento_por_volumen(self.__dict__[f'unidades_{planta}_usadas'])
         else:
             print("No es una caja asignable para el producto.")
-    
+        
     def revocar_producto(self, producto):
         if producto in self.productos_asignados:
             self.productos_asignados.remove(producto)
-            self.volumen_buenos_aires += producto.volumen_buenos_aires
-            self.volumen_curitiba += producto.volumen_curitiba
-            self.volumen_santiago += producto.volumen_santiago
-            self.volumen_monterrey += producto.volumen_monterrey
-            self.volumen_bakersfield += producto.volumen_bakersfield
+            plantas = ['buenos_aires', 'curitiba', 'santiago', 'monterrey', 'bakersfield']
+            for planta in plantas:
+                self.__dict__[f'unidades_{planta}_usadas'] -= producto.__dict__[f'produccion_{planta}']
+                self.__dict__[f'descuento_{planta}'] = calcular_descuento_por_volumen(self.__dict__[f'unidades_{planta}_usadas'])
         else: 
             print("El producto no usaba este tipo de caja.")
     
+    def costo_total(self):
+        costo = 0
+        plantas = ['buenos_aires', 'curitiba', 'santiago', 'monterrey', 'bakersfield']
+        
+        for planta in plantas:
+            unidades_usadas = getattr(self, f'unidades_{planta}_usadas')
+            descuento = getattr(self, f'descuento_{planta}')
+            precio_con_descuento = self.costo_unitario * (1 + descuento)
+            costo += unidades_usadas * precio_con_descuento
+        
+        return costo
+    
     def __repr__(self):
         return (f"<Caja {self.caja_id} | "
-                f"Int: {self.dim_interior_ancho}x{self.dim_interior_largo}x{self.dim_interior_alto}mm | "
-                f"Ext: {self.dim_exterior_ancho}x{self.dim_exterior_largo}x{self.dim_exterior_alto}mm | "
-                f"Grosor: {self.grosor_mm}mm>")
+                f"Int: {self.dim_interior_ancho}x{self.dim_interior_largo}x{self.dim_interior_alto}mm>")
