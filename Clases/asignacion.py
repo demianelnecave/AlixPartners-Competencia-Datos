376039db5c578413272dec84bbca112782e70554
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2026-07-23 17:26:18 -0300

    Correccion factibilidad

--- Clases/asignacion.py
+++ Clases/asignacion.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 class Asignacion:
     def __init__(self, producto, caja):
@@ -6,33 +7,30 @@ class Asignacion:
         self.caja = caja
 
     def validar_por_dimension(self):
-        maximo_en_10_porcent = (self.producto.dim_producto_alto * 1.1 >= self.caja.dim_interior_alto and
-                                self.producto.dim_producto_ancho * 1.1 >= self.caja.dim_interior_ancho and
-                                self.producto.dim_producto_largo * 1.1 >= self.caja.dim_interior_largo and
-                                self.producto.dim_producto_alto * 0.9 <= self.caja.dim_interior_alto and
-                                self.producto.dim_producto_ancho * 0.9 <= self.caja.dim_interior_ancho and
-                                self.producto.dim_producto_largo * 0.9 <= self.caja.dim_interior_largo)
+        caja_redimensionada = copy.deepcopy(self.caja)
+        caja_redimensionada.dim_interior_alto *= 1.1
+        caja_redimensionada.dim_interior_ancho *= 1.1
+        caja_redimensionada.dim_interior_largo *= 1.1
         
-        tope_40mm = (self.producto.dim_producto_alto + 40 >= self.caja.dim_interior_alto and
-                    self.producto.dim_producto_ancho + 40 >= self.caja.dim_interior_ancho and
-                    self.producto.dim_producto_largo + 40 >= self.caja.dim_interior_largo and
-                    self.producto.dim_producto_alto - 40 <= self.caja.dim_interior_alto and
-                    self.producto.dim_producto_ancho - 40 <= self.caja.dim_interior_ancho and
-                    self.producto.dim_producto_largo - 40 <= self.caja.dim_interior_largo)
-        
-        volumen_mayor = self.caja.volumen_interno() >= self.producto.volumen_producto()
-        return maximo_en_10_porcent and tope_40mm and volumen_mayor
+        volumen_mayor = caja_redimensionada.volumen_interno() >= self.producto.volumen_producto()
+        return volumen_mayor
     
     def validar_por_headspace(self):
         grosor = self.caja.grosor_mm
         headspace_por_grosor = {3: 0.06, 4.5: 0.08, 5: 0.1}
-        headspace = headspace_por_grosor[grosor]
+        headspace_max = headspace_por_grosor[grosor]
         
-        chequeo = (self.caja.dim_interior_alto * (1 - headspace) <= self.producto.dim_producto_alto and
-                   self.caja.dim_interior_ancho * (1 - headspace) <= self.producto.dim_producto_ancho and
-                   self.caja.dim_interior_largo * (1 - headspace) <= self.producto.dim_producto_largo)
+        headspace_alto = self.caja.dim_interior_alto - self.producto.dim_producto_alto
+        headspace_ancho = self.caja.dim_interior_ancho - self.producto.dim_producto_ancho
+        headspace_largo = self.caja.dim_interior_largo - self.producto.dim_producto_largo
         
-        return chequeo
+        chequeo_headspace = (headspace_alto / self.caja.dim_interior_alto <= headspace_max and
+                             headspace_ancho / self.caja.dim_interior_ancho <= headspace_max and
+                             headspace_largo / self.caja.dim_interior_largo <= headspace_max)
+        
+        tope_40mm = headspace_alto <= 40 and headspace_ancho <= 40 and headspace_largo <= 40
+        
+        return chequeo_headspace and tope_40mm
     
     def validar_por_resistencia(self):        
         carga_maxima = self.caja.carga_maxima()
